c62ae1c97912cae97b9ea4054570c237c2629ac9
Fawwaz Hosein <Fawwazhosein46@yahoo.com>
2021-01-21 15:27:01 -0400

    Improved Stripe Implementation2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -16,7 +16,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // 2) Create checkout session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      success_url: `${req.protocol}://${req.get('host')}/my-tours/`,
+      success_url: `${req.protocol}://${req.get('host')}/my-tours`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
       customer_email: req.user.email,
       client_reference_id: req.params.tourId,
@@ -64,8 +64,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({email: session.customer_email})).id;
-  const price = session.display_items[0].amount/100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({tour, user, price});
 }
 
@@ -74,23 +74,24 @@ const createBookingCheckout = async session => {
 
 
 exports.webhookCheckout = (req, res, next) => {
-  //1. Read Stripe signature out of the header
   const signature = req.headers['stripe-signature'];
+
   let event;
-    try{
-      //2. Creating Stripe event
-      event = stripe.webhooks.constructEvent(req.body, 
-      signature, process.env.STRIPE_WEBHOOK_SECRET);
-    }catch(err){
-      return res.status(400).send(`Webhook error: ${err.message}`);
-    }
-  
-    //If it is the event, we want to make the booking in DB
-    if(event.type === 'checkout.session.completed')
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
-    res.status(200).json({received: 'true'});
-}
+  res.status(200).json({ received: true });
+};
 
 
 

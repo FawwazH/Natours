1090af5f5c3c6d41109eb66ad7b4c308e896c3fa
Fawwaz Hosein <Fawwazhosein46@yahoo.com>
2021-01-21 11:58:01 -0400

    Fixed bug

--- server.js
+++ server.js
@@ -58,10 +58,10 @@ process.on('unhandledRejection', err => {
     });
 });
 
-// process.on('SIGTERM', ()=> {
-//     console.log('SIGTERM RECEIVED. Shutting down gracefully');
-//     server.close(() => {
-//         console.log('Process terminated!');
-//     })
-// })
+process.on('SIGTERM', ()=> {
+    console.log('SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+        console.log('Process terminated!');
+    })
+})
 
